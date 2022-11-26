96431f6d7b43ae62bfdc67d77595218b38bba5cf
Stiflerzak <hilarymwendia97@gmail.com>
2022-11-26 09:23:15 +0300

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+let names=["Guadalupe","Ollie","Aki"]
+const event="suprise"
+messages=[]
+
+function  writeCards(name,event){
+    for (let n =0;n<name.length;n++){
+        messages.push(`Thank you', ${names[n]}, for the wonderful ${event} gift!`);
+    }
+return messages;
+}
+function countDown(number){
+    while (number>= 0){
+        console.log(number--);
+    }
+}
